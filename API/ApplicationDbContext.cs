403e213e359cfb8d08b763061a881ad73e2f78c8
elijmcd <elijmcd@gmail.com>
2021-09-04 20:41:42 -0500

    first commit

---
+++ API/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNull] DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
